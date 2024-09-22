741f81f8e159909b7517f0d42f51513e4e2c7c66
ashiquzzaman77 <ashiquzzaman.rajib.cse@gmail.com>
2024-09-22 23:38:41 +0600

    Html Section Done Date 22-09-2024

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+//First Card
+document.getElementById('noakhali-donate').addEventListener('click', function () {
+
+    const noakhaliAmount = parseFloat(document.getElementById('noakhail-amount').value);
+
+    if (noakhaliAmount <= 0 || isNaN(noakhaliAmount)) {
+        document.getElementById('invild-error').classList.remove('hidden');
+        return;
+    }
+
+    const mainAccounts = parseFloat(document.getElementById('main-account').innerText);
+    const mainAccount = mainAccounts - noakhaliAmount;
+
+    document.getElementById('main-account').innerText = mainAccount.toFixed(2);
+    document.getElementById('amount').innerText = noakhaliAmount.toFixed(2);
+    
+    //Clear input filed
+    document.getElementById('noakhail-amount').value = '';
+
+    // Show alert message
+    alert(`Amount added: ${noakhaliAmount.toFixed(2)} BDT Successfully`);
+ 
+
+});
